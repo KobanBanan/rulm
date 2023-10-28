c94379a47ccd26ea8ece99d0b91d1c5bb4ed129d
alexeynikitin <abuseofinnocent@gmail.com>
2023-10-28 14:39:39 +0700

    logic for dialogue train

--- self_instruct/src/dataset.py
+++ self_instruct/src/dataset.py
@@ -58,8 +58,8 @@ class ChatDataset(Dataset):
         )["input_ids"]
 
     def convert_record(self, record):
-        print(record)
-        if record['search']:
+        # check if it's search or dialogue
+        if record['messages'][0]['search']:
             conversation = Conversation.from_template(self.templates_path)
         else:
             conversation = Conversation.from_template(self.templates_path, is_search=False)
