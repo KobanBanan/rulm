d716d9e06a1f55beb5634f31d1b14b6e891813c7
alexeynikitin <abuseofinnocent@gmail.com>
2023-12-08 11:23:28 +0700

    OpenHermes-2.5-Mistral-7B

--- self_instruct/src/util/chat.py
+++ self_instruct/src/util/chat.py
@@ -69,6 +69,7 @@ class Conversation:
             return self.system_message_template.format(**message)
         if message["role"] == self.user_role:
             return self.user_message_template.format(**message)
+        print(self.bot_message_template)
         return self.bot_message_template.format(**message)
 
     def get_prompt(self, tokenizer, max_tokens: int = None, add_suffix: bool = False):
